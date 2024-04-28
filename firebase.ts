70bce85fa97d0ea58af367c56c00e2ea73194b66
otherRick <drckbr@gmail.com>
2024-04-28 11:26:46 -0300

    add firebase config

--- firebase.ts
+++ firebase.ts
@@ -1,15 +1,17 @@
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
 
+const fbConfig = import.meta.env;
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyB8WRG7TB-S6cQRUHtmRIBG2wutSqZu4PQ',
-  authDomain: 'mkssistema-706df.firebaseapp.com',
-  projectId: 'mkssistema-706df',
-  storageBucket: 'mkssistema-706df.appspot.com',
-  messagingSenderId: '350929328153',
-  appId: '1:350929328153:web:df4237f1cdf80cd353b288',
-  measurementId: 'G-DB6JF0S5FG'
+  apiKey: fbConfig.VITE_FIREBASE_API_KEY_DEV,
+  authDomain: fbConfig.VITE_FIREBASE_AUTH_DOMAIN_DEV,
+  projectId: fbConfig.VITE_FIREBASE_PROJECT_ID_DEV,
+  storageBucket: fbConfig.VITE_FIREBASE_STORAGE_BUCKET_DEV,
+  messagingSenderId: fbConfig.VITE_FIREBASE_MESSAGING_SENDER_ID_DEV,
+  appId: fbConfig.VITE_FIREBASE_APP_ID_DEV,
+  measurementId: fbConfig.VITE_FIREBASE_MEASUREMENT_ID_DEV
 };
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
